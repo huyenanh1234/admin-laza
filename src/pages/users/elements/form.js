274e338b6ff466de32d8792ebfe86aa6de4cdf17
huyenanh1234 <batthuong96@gmail.com>
2023-08-31 22:01:43 +0700

    api-user-CRUD

--- src/pages/users/elements/form.js
+++ src/pages/users/elements/form.js
@@ -22,15 +22,16 @@ export default function UserFormElement({isUpdate = false})
     });
     let urlParams = useParams();
     let navigate = useNavigate();
-    console.log(urlParams);
+    //console.log(urlParams);
     useEffect(() => {
         if (isUpdate) {
             (
                 async () => {
                     const userResponse = await userApis.show(urlParams.userId);
-                    console.log(11);
+                    console.log(urlParams.userId);
+                    console.log(userResponse);
                     if (userResponse.success) {
-                        setValue('avata', userResponse.data.name)
+                        setValue('avata', userResponse.data.avatar)
                         setValue('name', userResponse.data.name)
                         setValue('email', userResponse.data.email)
                         setValue('phone', userResponse.data.phone)
